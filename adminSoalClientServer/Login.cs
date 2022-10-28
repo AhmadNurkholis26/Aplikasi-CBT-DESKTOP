dc9092879b5e4d74ea185b88bde1921767e18331
ahmad <ahmad@LAPTOP-BARE0UKL>
2022-10-28 13:52:32 +0700

    Add project files.

---
+++ adminSoalClientServer/Login.cs
@@ -0,0 +1,37 @@
+
+using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace adminSoalClientServer
+{
+	public class Login
+	{
+		MySqlCommand komand;
+		MySqlDataAdapter adapter;
+		koneksi classKoneksi;
+		string sql;
+		DataTable tabel;
+		
+		public DataTable ValidasiLogin(string user, string pass)
+		{
+			classKoneksi = new koneksi();
+			sql = "call loginAdmin('"+user+"','"+pass+"')";
+			tabel = new DataTable();
+			
+			try 
+			{
+				classKoneksi.koneksiBuka();
+				komand = new MySqlCommand(sql,classKoneksi.konek);
+				adapter = new MySqlDataAdapter(komand);
+				adapter.Fill(tabel);
+			} catch (Exception) 
+			{
+				
+			}
+			classKoneksi.koneksiTutup();
+			return tabel;
+			
+		}
+	}
+}
---
+++ soalClientServer/soalClientServer/Login.cs
@@ -0,0 +1,38 @@
+
+using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace soalClientServer
+{
+	
+	public class Login
+	{
+		MySqlCommand komand;
+		MySqlDataAdapter adapter;
+		koneksi classKoneksi;
+		string sql;
+		DataTable tabel;
+		
+		public DataTable ValidasiLogin(string user, string pass)
+		{
+			classKoneksi = new koneksi();
+			sql = "call loginUser('"+user+"','"+pass+"')";
+			tabel = new DataTable();
+			
+			try 
+			{
+				classKoneksi.koneksiBuka();
+				komand = new MySqlCommand(sql,classKoneksi.konek);
+				adapter = new MySqlDataAdapter(komand);
+				adapter.Fill(tabel);
+			} catch (Exception) 
+			{
+				
+			}
+			classKoneksi.koneksiTutup();
+			return tabel;
+			
+		}
+	}
+}
